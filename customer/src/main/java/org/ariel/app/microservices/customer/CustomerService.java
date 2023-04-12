2dca7baf49ea216a42aff25d77bf72e51d3468da
Ariel Massetti <ariel@finconecta.com>
2023-04-11 22:21:50 -0300

    Adding Eureka server for service discovery

--- customer/src/main/java/org/ariel/app/microservices/customer/CustomerService.java
+++ customer/src/main/java/org/ariel/app/microservices/customer/CustomerService.java
@@ -28,7 +28,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse response = restTemplate.getForObject(
-                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
